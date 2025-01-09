ac432bebcf3f12cccd6c5d9e6064904a72eafa95
learnuidev <learnuidev@gmail.com>
2025-01-09 13:40:39 -0500

    added basic pages

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,34 +1,79 @@
 "use client";
 
 import Link from "next/link";
-import { Moon, Sun } from "lucide-react";
+import {
+  Moon,
+  Sun,
+  Library,
+  Wand,
+  Brain,
+  BriefcaseBusiness,
+} from "lucide-react";
 import { useTheme } from "next-themes";
+import { cn } from "@/lib/utils";
 
 function NavMobile() {
   return (
     <nav className="block sm:hidden">
-      <Link href="/" className="font-bold">
-        VG
-      </Link>
+      <Link href="/">VG</Link>
     </nav>
+  );
+}
+
+function NavItemLink({
+  children,
+  title,
+  href,
+}: {
+  children: React.ReactNode;
+  href: string;
+  title: string;
+}) {
+  return (
+    <Link
+      href={href}
+      className={cn("flex flex-col items-center hover:text-rose-400")}
+    >
+      <span className="uppercase text-sm">{title}</span>
+    </Link>
+  );
+
+  return (
+    <Link
+      href={href}
+      className={cn("flex flex-col items-center dark:hover:text-white")}
+    >
+      {children}
+      <span className="text-[10px] mt-[2px] uppercase">{title}</span>
+    </Link>
   );
 }
 
 function NavDesktop() {
   return (
-    <nav className="hidden sm:block space-x-8">
-      <Link href="/" className="font-bold">
-        Library
-      </Link>
-      <Link href="/" className="font-bold">
-        Courses
-      </Link>
-      <Link href="/" className="font-bold">
-        With With Me
-      </Link>
-      <Link href="/" className="font-bold">
-        Contact
-      </Link>
+    <nav className="hidden sm:block">
+      <div className="flex space-x-12">
+        <NavItemLink title="Library" href="/library">
+          <span>
+            <Library />
+          </span>
+        </NavItemLink>
+        <NavItemLink title="Courses" href="/courses">
+          <span>
+            <Brain />
+          </span>
+        </NavItemLink>
+        <NavItemLink title="Projects" href="/projects">
+          <span>
+            <Wand />
+          </span>
+        </NavItemLink>
+        <NavItemLink title="Work With Me" href="/work">
+          <span>
+            <BriefcaseBusiness />
+          </span>
+        </NavItemLink>
+      </div>
     </nav>
   );
 }
@@ -36,7 +81,7 @@ function NavDesktop() {
 export const NavBar = () => {
   const { setTheme, theme } = useTheme();
   return (
-    <header className="flex justify-between">
+    <header className="flex justify-between my-4">
       {" "}
       <Link href="/" className="font-bold hidden sm:block">
         VG
