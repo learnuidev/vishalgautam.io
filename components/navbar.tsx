0265c501896ac1bf34936f37a7b6fec55f1fa8df
learnuidev <learnuidev@gmail.com>
2025-01-09 20:44:05 -0500

    added use-sound

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import useSound from "use-sound";
 
 import Link from "next/link";
 import {
@@ -8,12 +9,15 @@ import {
   Wand,
   Brain,
   BriefcaseBusiness,
+  Volume2,
+  Volume,
 } from "lucide-react";
 import { useTheme } from "next-themes";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 function NavMobile() {
   return (
@@ -81,6 +85,16 @@ function NavDesktop() {
 
 export const NavBar = () => {
   const { setTheme, theme } = useTheme();
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const switchOffUrl = `https://nomadmethod-api-dev-assetsbucket-2u2iqsv5nizc.s3.us-east-1.amazonaws.com/learnuidev@gmail.com/01JH6X1JMEACXMV4CY8ZSHTC0D.m4a`;
+  const switchOnUrl = `https://nomadmethod-api-dev-assetsbucket-2u2iqsv5nizc.s3.us-east-1.amazonaws.com/learnuidev@gmail.com/01JH6X19QJ68E06NA827N5WSTQ.m4a`;
+
+  const musicUrl =
+    "https://nomadmethod-api-dev-assetsbucket-2u2iqsv5nizc.s3.us-east-1.amazonaws.com/learnuidev@gmail.com/01JH6XXBGT3NXBE50425NY5YNY.mp3";
+  const [playMusic, { pause }] = useSound(musicUrl);
+  const [playSwitchOff] = useSound(switchOffUrl);
+  const [playSwitchOn] = useSound(switchOnUrl);
 
   return (
     <header className="flex justify-between my-4 items-center">
@@ -95,7 +109,27 @@ export const NavBar = () => {
       <div className="space-x-6 flex items-center">
         <button
           onClick={() => {
-            setTheme((prev) => (prev === "light" ? "dark" : "light"));
+            if (isPlaying) {
+              pause();
+              setIsPlaying(false);
+            } else {
+              playMusic();
+              setIsPlaying(true);
+            }
+          }}
+        >
+          {isPlaying ? <Volume /> : <Volume2 />}
+        </button>
+        <button
+          onClick={() => {
+            setTheme((prev) => {
+              if (prev === "light") {
+                playSwitchOff();
+                return "dark";
+              }
+              playSwitchOn();
+              return "light";
+            });
           }}
         >
           {theme === "light" ? <Moon /> : <Sun />}
