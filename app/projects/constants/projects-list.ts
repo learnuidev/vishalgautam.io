8d6e75cd504648f9a0bafb17b6f97588ceae8922
learnuidev <learnuidev@gmail.com>
2025-02-05 12:40:25 -0500

    wip

---
+++ app/projects/constants/projects-list.ts
@@ -0,0 +1,20 @@
+export const projectsList = [
+  {
+    id: "myelin",
+    title: "Myelin",
+    description: "Translation Infra that sparks joy",
+    link: "https://www.myelin.dev",
+  },
+  {
+    id: "mandarino",
+    title: "Mandaino",
+    description: "Next gen language learning app",
+    link: "https://www.mandarino.io",
+  },
+  //   {
+  //     id: "dyno",
+  //     title: "Dyno",
+  //     description: "DynamoDB client from the future",
+  //     link: "https://www.dyno.im",
+  //   },
+];
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,35 @@
-import { Nothing } from "@/components/nothing";
+import { projectsList } from "./constants/projects-list";
+import Link from "next/link";
 
 export default function Projects() {
   return (
     <div className="mt-40">
       <h1 className="text-2xl font-bold mb-12">Projects</h1>{" "}
-      <section>
-        <Nothing message={"Coming Soon"} />
+      <section className="grid sm:grid-cols-2 grid-cols-1 gap-4">
+        {projectsList.map((project) => {
+          return (
+            <div
+              key={project.id}
+              className="rounded-2xl p-4 bg-gray-100 dark:bg-[rgb(22,23,24)]"
+            >
+              <div>
+                <h2 className="text-xl font-bold">{project.title}</h2>
+                <p className=" text-gray-500 font-light">
+                  {" "}
+                  {project.description}
+                </p>
+              </div>
+
+              <Link
+                className="rounded-full text-sm mt-4 inline-block dark:bg-white bg-black dark:text-black text-white px-4 py-1"
+                href={project.link}
+                target="_blank"
+              >
+                View
+              </Link>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
