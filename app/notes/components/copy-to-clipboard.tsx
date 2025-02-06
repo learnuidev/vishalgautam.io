516a94d70f714ed5d89ffa63e5a86b0ce674fbaf
learnuidev <learnuidev@gmail.com>
2025-02-05 22:35:36 -0500

    added ux

--- app/notes/components/copy-to-clipboard.tsx
+++ app/notes/components/copy-to-clipboard.tsx
@@ -1,17 +1,29 @@
 "use client";
 import { copyTextToClipboard } from "@/libs/misc/copy-text-to-clipboard";
-import { CopyIcon } from "lucide-react";
+import { CheckIcon, CopyIcon } from "lucide-react";
+import { useState } from "react";
 
 export const CopyToClipboard = ({ text }: { text: string }) => {
+  const [showSucces, setShowSuccess] = useState(false);
   return (
     <div className="flex justify-end mb-4">
       <button
-        className=""
+        className="hover:text-gray-400 transition-all"
         onClick={() => {
-          copyTextToClipboard(text);
+          copyTextToClipboard(text).then(() => {
+            setShowSuccess(true);
+
+            setTimeout(() => {
+              setShowSuccess(false);
+            }, 2000);
+          });
         }}
       >
-        <CopyIcon />
+        {showSucces ? (
+          <CheckIcon size={20} className="text-green-500" />
+        ) : (
+          <CopyIcon size={20} />
+        )}
       </button>
     </div>
   );
