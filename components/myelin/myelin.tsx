6396c5f8b144139aa01872c5426d8f866799dbba
learnuidev <learnuidev@gmail.com>
2025-02-08 12:15:21 -0500

    wip

--- components/myelin/myelin.tsx
+++ components/myelin/myelin.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { useListTranslationsQuery } from "./queries/use-list-translations-query";
+import myelinConfig from "@/myelin.config.json";
+
+// function getNamespaces() {}
 
 export const Myelin = ({ className }: { className?: string }) => {
   const { data } = useListTranslationsQuery();
   return (
     <div className={className}>
-      <h1>Myelin</h1>
+      <h1 className="text-center font-bold">
+        Myelin - <span>{myelinConfig?.projectId}</span>
+      </h1>
+
+      {/* <Tabs></Tabs> */}
 
       <div>
         <code>
