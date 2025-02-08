807f95cb1357a31204f34dadb179f7e08402cec8
learnuidev <learnuidev@gmail.com>
2025-02-08 12:58:57 -0500

    wip

--- app/api/myelin/list-translations/route.ts
+++ app/api/myelin/list-translations/route.ts
@@ -3,6 +3,10 @@ import { listTranslations } from "myelino";
 
 export async function GET() {
   try {
+    if (process.env.NODE_ENV === "production") {
+      throw new Error("This is a development feature only");
+    }
+
     const translations = await listTranslations();
 
     return Response.json(translations);
